6f531a603bd728ac8a38ed54ac273e0837fcc577
Bernard Banta <banta.bernard@gmail.com>
2025-04-19 00:09:32 +0300

    Attempting to run ussd service in the background

---
+++ app/src/main/java/com/sanarei/sanareimobileapp/UssdAccessibilityService.kt
@@ -0,0 +1,19 @@
+package com.sanarei.sanareimobileapp
+
+import android.accessibilityservice.AccessibilityService
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+
+class UssdAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        val source = event?.source ?: return
+
+        val text = source.text//?.joinToString(" ") ?: ""
+        if (text.isNotEmpty()) {
+            Log.d("USSD_RESPONSE", "USSD Text: $text")
+            // You could broadcast this back to your app or show it in a UI
+        }
+    }
+
+    override fun onInterrupt() {}
+}
